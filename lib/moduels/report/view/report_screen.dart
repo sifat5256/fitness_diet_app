55ede51760a797a205c207a4fd68ca8adc723189
Asus <iqramulhasan65@gmail.com>
2025-01-26 09:51:04 +0600

    update2

--- lib/moduels/report/view/report_screen.dart
+++ lib/moduels/report/view/report_screen.dart
@@ -83,112 +83,103 @@ class _ReportScreenState extends State<ReportScreen> {
             const SizedBox(
               height: 10,
             ),
-            Container(
+            SizedBox(
               height: 170,
-              child: Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 10,
-                    itemBuilder: (context,index){
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 10),
-                      child: Container(
-
-                        height: 170,
-                        width: MediaQuery.of(context).size.width*0.7,
-                        decoration: BoxDecoration(
-                          color: Colors.blue.shade100,
-                          borderRadius: BorderRadius.circular(18),
-
-                        ),
-                        child:  Row(
-                          children: [
-
-                             Expanded(
-                              flex:3,
-                              child: Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("Total Body Yoga Deep Stretch",
-                                      style: TextStyle(
-                                          fontSize: 22,
-                                          fontWeight: FontWeight.bold
-                                      ),
-                                    ),
-                                    Row(
-                                      children: [
-                                        Icon(Icons.timelapse,color: Colors.grey,),
-                                        Text("10:15",style: TextStyle(
-                                          color: Colors.grey
-                                        ),),
-                                        SizedBox(width: 10,),
-                                        Icon(Icons.local_fire_department,color: Colors.grey,),
-                                        Text("390 kcl",style: TextStyle(
-                                            color: Colors.grey
-                                        ),),
-                                      ],
-                                    ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                GestureDetector(
-                                  onTap: (){
-
-                                  },
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                                    child: Material(
-                                      elevation: 2,
-                                      borderRadius: BorderRadius.circular(14),
-                                      child: Container(
-                                        height: 40,
-
-                                        width: 80,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(20),
-                                          color: Colors.white,
-                                        ),
-                                        child: Row(
-                                          mainAxisAlignment: MainAxisAlignment.center,
-                                          crossAxisAlignment: CrossAxisAlignment.center,
-                                          children: [
-
-                                            Text(
-                                              "Play",
-                                              style: TextStyle(
-                                                color: Colors.black,
-                                                fontSize: 16.0,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                            Icon(Icons.play_arrow)
-                                          ],
-                                        ),
-                                      ),
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade100,
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      child: Row(
+                        children: [
+                          Flexible(
+                            flex: 3,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Total Body Yoga Deep Stretch",
+                                    style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
                                     ),
                                   ),
-                                )
-
-
-                                  ],
-
-                                ),
+                                  Row(
+                                    children: [
+                                      Icon(Icons.timelapse, color: Colors.grey),
+                                      Text(
+                                        "10:15",
+                                        style: TextStyle(color: Colors.grey),
+                                      ),
+                                      SizedBox(width: 10),
+                                      Icon(Icons.local_fire_department, color: Colors.grey),
+                                      Text(
+                                        "390 kcl",
+                                        style: TextStyle(color: Colors.grey),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 10),
+                                  GestureDetector(
+                                    onTap: () {},
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                                      child: Material(
+                                        elevation: 2,
+                                        borderRadius: BorderRadius.circular(14),
+                                        child: Container(
+                                          height: 40,
+                                          width: 80,
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(20),
+                                            color: Colors.white,
+                                          ),
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            crossAxisAlignment: CrossAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                "Play",
+                                                style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 16.0,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              Icon(Icons.play_arrow),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
                               ),
                             ),
-                            Expanded(
-                                flex:2,
-                                child: Image.asset("lib/utils/assets/images/bkyouga.png",)),
-                          ],
-                        ),
+                          ),
+                          Flexible(
+                            flex: 2,
+                            child: Image.asset("lib/utils/assets/images/bkyouga.png"),
+                          ),
+                        ],
                       ),
-                    );
-
-                    }),
+                    ),
+                  );
+                },
               ),
             ),
+
 
 
           ],
